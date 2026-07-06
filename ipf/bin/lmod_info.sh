2f7a80e235bf3f7aa0048e5f21da1d63672ad96e
Andy Loftus <aloftus@gmail.com>
2026-07-06 21:46:34 +0000

    fixup lmod_info

--- ipf/bin/lmod_info.sh
+++ ipf/bin/lmod_info.sh
@@ -4,8 +4,18 @@ INSTALL_DIR=___INSTALL_DIR___
 
 . ${INSTALL_DIR}/lib/utils.sh
 
-MODULE=$( which module )
-[[ -x "${MODULE}" ]] || die 'module command not found'
+find_module() {
+  local _type
+  _type=$( type -t module )
+  case "${_type}" in
+    function) MODULE=module;;
+    file)
+      MODULE=$( which module )
+      [[ -x "${MODULE}" ]] || die "module command '${MODULE}' not executable"
+      ;;
+    *) die 'module command not found'
+  esac
+}
 
 JQ=$( which jq )
 [[ -x "${JQ}" ]] || die 'jq command not found'
@@ -16,7 +26,8 @@ module_config_as_json() {
   # module config prints output on stderr, so have be careful when gathering it
   [[ -s "${TMP}" ]] \
   || "${MODULE}" --config_json 1>/dev/null 2>"${TMP}"
-  cat "${TMP}"
+  # skip any non-json lines from the file
+  cat "${TMP}" | sed -e '/^[^{]/ d'
 }
 
 
