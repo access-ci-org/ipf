111dedad15bd5b8f21aa3844e27715875d81f0fb
Andy Loftus <aloftus@gmail.com>
2026-07-06 20:42:40 +0000

    CTT-1033 lmod 9.2.2 support

---
+++ ipf/bin/lmod_info.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+INSTALL_DIR=___INSTALL_DIR___
+
+. ${INSTALL_DIR}/lib/utils.sh
+
+MODULE=$( which module )
+[[ -x "${MODULE}" ]] || die 'module command not found'
+
+JQ=$( which jq )
+[[ -x "${JQ}" ]] || die 'jq command not found'
+
+TMP=$(mktemp)
+
+module_config_as_json() {
+  # module config prints output on stderr, so have be careful when gathering it
+  [[ -s "${TMP}" ]] \
+  || "${MODULE}" --config_json 1>/dev/null 2>"${TMP}"
+  cat "${TMP}"
+}
+
+
+version_info() {
+  # keys='["tcl_version", "luaV", "lmodV"]'
+  # module_config_as_json \
+  # | "${JQ}" \
+  #   --argjson keys "${keys}" \
+  #   '.configT | with_entries(select(.key as $k | $keys | index($k)))'
+
+  module_config_as_json \
+  | "${JQ}" '.configT | {lmodV, tcl_version, luaV}'
+}
+
+
+cache_info() {
+  module_config_as_json \
+  | "${JQ}" '.cache'
+}
+
+
+cleanup() {
+  rm -f "${TMP}"
+}
+
+###
+# MAIN
+###
+
+version_info
+
+cache_info
+
+cleanup
