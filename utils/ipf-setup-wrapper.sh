54af0109760a826f8a3fb8bec5834c1fea8d2693
Andy Loftus <aloftus@gmail.com>
2026-07-09 04:23:27 +0000

    update setup wrapper for easy custom installs

--- utils/ipf-setup-wrapper.sh
+++ utils/ipf-setup-wrapper.sh
@@ -2,30 +2,46 @@
 
 set -x
 
-IPF_BIN="${HOME}"/ipf/bin
-VENV="${HOME}"/ipf/.venv
-IPF_SETUP="${HOME}"/ipf-setup.sh
+# =========================
+# customize these as needed
+# =========================
 
-[[ -z "${IPF_GIT_BRANCH}" ]] && {
-  echo "Missing environment variable IPF_GIT_BRANCH"
-  exit 1
-}
-URL_BASE='https://raw.githubusercontent.com/access-ci-org/ipf/refs/heads'
-IPF_SETUP_URL="${URL_BASE}"/"${IPF_GIT_BRANCH}"/setup.sh
-
-bash "${IPF_BIN}"/wfm stop
-
-rm -rf ~/ipf
-
-rm -f "${IPF_SETUP}"
-
-curl -o "${IPF_SETUP}" "${IPF_SETUP_URL}"
-
-export IPF_UV_URL="${URL_BASE}"/"${IPF_GIT_BRANCH}"/utils/uv_installer.sh
+# set to an absolute path or leave unset (defaults to $HOME/ipf)
+export IPF_INSTALL_DIR=/scratch/user/u.al8635/ipf
+# set to "yes" or leave unset (defaults to latest)
 export IPF_ALLOW_PRE_RELEASE=yes
+# get version string from
+#    https://pypi.org/project/ipf/#history      (production releases)
+# OR https://test.pypi.org/project/ipf/#history (pre-release versions)
+export IPF_INSTALL_VERSION=1.9.9.dev1783568317
+# either a branch name or leave unset (defaults to master)
+IPF_GIT_BRANCH=ATS-30367
+# where to save the installer script
+IPF_SETUP="${HOME}"/ipf-setup.sh
+# get a list of commands that can be run manually
+#MANUAL=yes
 
-bash "${IPF_SETUP}"
+# =====================================
+# shouldn't need to touch anything else
+# =====================================
 
-"${VENV}"/bin/python --version
+IPF_BIN="${IPF_INSTALL_DIR}"/bin
+VENV="${IPF_INSTALL_DIR}"/.venv
+URL_BASE='https://raw.githubusercontent.com/access-ci-org/ipf/refs/heads'
+IPF_SETUP_URL="${URL_BASE}"/"${IPF_GIT_BRANCH:-master}"/setup.sh
+ACTION="${MANUAL:+echo}"
 
-"${VENV}"/bin/python -m pip freeze
+[[ -d "${IPF_BIN}" ]] && {
+  ${ACTION}" bash "${IPF_BIN}"/wfm stop
+  ${ACTION}" rm -rf "${IPF_INSTALL_DIR}"/
+}
+
+${ACTION}" rm -f "${IPF_SETUP}"
+
+${ACTION}" curl -o "${IPF_SETUP}" "${IPF_SETUP_URL}"
+
+${ACTION}" bash "${IPF_SETUP}"
+
+${ACTION}" "${VENV}"/bin/python --version
+
+${ACTION}" "${VENV}"/bin/python -m pip freeze
